c57a91f8e01009e23165330858de90c81fe863c0
Alvian Naya Pradhana <alvinnaya13@gmail.com>
2024-11-05 21:39:46 -0800

    add ui: add search bar

--- Semantic-car-search/src/App.vue
+++ Semantic-car-search/src/App.vue
@@ -4,17 +4,36 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="w-full min-h-screen flex flex-col justify-center items-center bg-main">
+    <h1 class="text-5xl">Hello</h1>
+    <div class="flex flex-nowrap m-4 gap-2 items-center h-[45px] w-full justify-center ">
+      <div class=" h-full bg-slate-300 rounded-xl w-[45rem]
+       bg-secondary overflow-hidden shrink border border-stone-800 flex flex-nowrap items-center">
+        <div class="h-[60%] flex items-center mx-3 opacity-65">
+          <img class=" h-[80%] m-auto" src="./assets/search.svg">
+        </div>
+        <input type="text" placeholder="Masukkan sesuatu... " class="p-2 text-xl outline-none w-full bg-secondary">
+        <div class="w-[2.5rem] h-[2rem] bg-orange mx-4 rounded-md">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        </div>
+       
+
+      </div>
+
+      <div class="h-full bg-orange p-4 rounded-lg flex justify-center items-center text-xl font-semibold gap-1">
+        <div class="h-[1.2rem] w-[1.2rem] flex items-center opacity-65">
+          <img class=" h-full m-auto" src="./assets/search.svg">
+        </div>
+        <p class="hidden md:flex">
+          search  
+        </p>
+        
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    
+    
+  </div>
+  
 </template>
 
 <style scoped>
---
+++ Semantic-car-search/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      'main': '#fafaf9',
+      'secondary': '#f5f5f4',
+      'orange': '#fed7aa',
+      
+    },
+  },
+  plugins: [],
+}
+
